d6c7297ca02f6daf6726e2d67f8b19ea478c36c4
Kevin Lazich <dev@kevinlazich.com>
2018-12-21 12:00:17 -0500

    Complete problem 6

--- problems/6/index.js
+++ problems/6/index.js
@@ -2,6 +2,12 @@ export const problem = 6
 export const statement =
   'Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.'
 
-function answer() {}
+const sumNtr = n => (n * (n + 1)) / 2
+const sumNtrSqr = n => (n * (n + 1) * (2 * n + 1)) / 6
 
-export const result = null
+function answer() {
+  const n = 100
+  return sumNtr(n) ** 2 - sumNtrSqr(n)
+}
+
+export const result = answer()
