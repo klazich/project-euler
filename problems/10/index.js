fa4caa6c5ea89061cbdb00b5aefeac796b5e7d8d
Kevin Lazich <dev@kevinlazich.com>
2018-12-21 13:39:40 -0500

    Complete problem 10

--- problems/10/index.js
+++ problems/10/index.js
@@ -1,4 +1,15 @@
 export const problem = 10
 export const statement = 'Find the sum of all the primes below two million.'
-function answer() {}
-export const result = null
+
+import { primes } from '../helpers'
+
+function answer() {
+  const iter = primes()
+  let sum = 0
+  for (let prime of iter) {
+    if (prime >= 2000000) return sum
+    sum += prime
+  }
+}
+
+export const result = 142913828922
