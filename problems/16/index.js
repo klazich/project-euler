9e3e386c813253b105ba1789917cc018d005cf14
Kevin Lazich <dev@kevinlazich.com>
2018-12-27 14:40:22 -0500

    Complete problem 16

--- problems/16/index.js
+++ problems/16/index.js
@@ -1,4 +1,15 @@
 export const problem = 16
 export const statement = 'What is the sum of the digits of the number 2^1000?'
-function answer() {}
-export const result = null
+
+const largeNumber = 2 ** 1000
+const sum = (a, b) => a + b
+
+function answer() {
+  return BigInt(largeNumber)
+    .toString()
+    .split('')
+    .map(l => parseInt(l))
+    .reduce(sum, 0)
+}
+
+export const result = 1366
