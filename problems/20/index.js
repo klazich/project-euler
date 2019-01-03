189bd193bfb8d3435bef068681a92cf6c4e8c662
Kevin Lazich <dev@kevinlazich.com>
2019-01-03 08:56:32 -0500

    Complete problem 20

--- problems/20/index.js
+++ problems/20/index.js
@@ -1,4 +1,26 @@
 export const problem = 20
 export const statement = 'Find the sum of the digits in the number 100!'
-function answer() {}
-export const result = answer()
+
+function factorialRec(n) {
+  return n === 0 ? 1 : n * factorial(n - 1)
+}
+
+function factorial(n) {
+  let acc = BigInt(1)
+  let i = BigInt(n)
+  while (i > 0) {
+    acc = acc * i--
+  }
+  return acc
+}
+
+function answer() {
+  let factors = factorial(100)
+  let digits = `${factors}`
+    .replace('n', '')
+    .split('')
+    .map(e => parseInt(e))
+  return digits.reduce((p, c) => p + c)
+}
+
+export const result = 648
